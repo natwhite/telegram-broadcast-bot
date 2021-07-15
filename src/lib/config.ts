7816e5e76c46cd313336be00b44a34072c270c44
nwhite <git@nathaniel.sh>
2021-07-15 15:43:21 -0700

    Remove unused fields from Configuration class.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,15 +2,9 @@ import {FileService} from '../services/file.service';
 
 const configFile = 'config.json';
 
-export interface AuthorizedAdmin {
-  id: number;
-  name: string;
-}
-
 export class Configuration {
   static indent = 4;
   static botToken: string;
-  static authorizedAdmins: AuthorizedAdmin[];
 
   static async loadConfig(): Promise<Configuration> {
     const configuration = await FileService.loadFile<Configuration>(configFile);
