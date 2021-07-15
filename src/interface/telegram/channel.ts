4e516d3a90cffd3769fd8c32e2d734c23daf8d1a
nwhite <git@nathaniel.sh>
2021-07-15 15:44:28 -0700

    Extend Chanel class to allow for the adding and removing of authorized users. Added static fromContext function to allow simple channel creation from Grammy Context.

--- src/interface/telegram/channel.ts
+++ src/interface/telegram/channel.ts
@@ -1,4 +1,7 @@
+import {Chat, Message} from 'grammy/out/platform';
 import {Client} from './client';
+import {User} from './user';
+import {Context} from 'grammy';
 
 export type ChannelType = 'group';
 
@@ -8,13 +11,33 @@ export class Channel {
   dateCreated: Date;
   title: string;
   type: ChannelType;
+  // TODO : We could use a map here for quicker indexing, but this array is expected to have less than 10 entries, so it's not needed.
+  authorizedUsers: User[];
+  public lastKeyboardMessageId?: number;
 
-
-  constructor(id: number, title: string, type: ChannelType, client?: Client) {
+  constructor(id: number, title: string, type: ChannelType, client?: Client, authorizedUsers: User[] = []) {
     this.id = id;
     this.title = title;
     this.type = type;
     this.dateCreated = new Date();
     if (client) this.client = client;
+    this.authorizedUsers = authorizedUsers;
+  }
+
+  public addAuthorizedUser(user: User) {
+    this.authorizedUsers.push(user);
+  }
+
+  public removeAuthorizedUsers(userToRemove: User) {
+    this.authorizedUsers = this.authorizedUsers.filter(user => user.id == userToRemove.id);
+  }
+
+  public static fromContext({message}: Context): Channel {
+    if (!message) throw new Error('No message defined for received context, this shouldn\'t happen.');
+
+    const {id, title, type} = message.chat as Chat.GroupChat;
+    console.log(`Creating channel from context ${id}, ${title}, ${type}`);
+
+    return new Channel(id, title, type);
   }
 }
