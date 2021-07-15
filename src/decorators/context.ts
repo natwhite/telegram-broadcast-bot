1193c855eb74d1e0cb04c0ff89fc712fcb30f789
nwhite <git@nathaniel.sh>
2021-07-15 15:42:55 -0700

    Create centralized context decorator export file to simplify imports.

---
+++ src/decorators/context.ts
@@ -0,0 +1,13 @@
+import {AdminChannelOnly} from './context/admin.channel.only';
+import {ClientChannelOnly} from './context/client.channel.only';
+import {AuthorizedCommand} from './context/authorized.command';
+import {TrackedServersOnly} from './context/tracked.servers.only';
+import { GlobalAdminOnly } from './context/global.admins.only';
+
+export {
+  AuthorizedCommand,
+  GlobalAdminOnly,
+  TrackedServersOnly,
+  ClientChannelOnly,
+  AdminChannelOnly,
+};
