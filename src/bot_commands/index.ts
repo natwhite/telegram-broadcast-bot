0e1e6bcb6a4b6293c05dcbdb2b2d0428b3726b3d
nwhite <git@nathaniels.email>
2021-12-25 12:16:33 -0800

    Add distpackage.json to vcs

--- src/bot_commands/index.ts
+++ src/bot_commands/index.ts
@@ -1,17 +1,17 @@
 import {AddChatToGroup} from './add.chat.to.group';
 import {AuthorizeChat} from './authorize.chat';
-import {InitializeChat} from './initialize.chat';
 import {AuthorizeUsers} from './authorize.users';
-import {UnauthorizeChat} from './unauthorize.chat';
 import {BroadcastMessage} from './broadcast.message';
+import {UnauthorizeChat} from './unauthorize.chat';
+import {InitializeChat} from './initialize.chat';
 import {ShowCommands} from './show.commands';
 
 export {
   AddChatToGroup,
-  AuthorizeUsers,
   AuthorizeChat,
+  AuthorizeUsers,
   BroadcastMessage,
-  InitializeChat,
   UnauthorizeChat,
+  InitializeChat,
   ShowCommands
 }
